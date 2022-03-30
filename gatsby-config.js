5d89083d9bf21eba9e1ca396b7774fb8ac7c415c
Artem Busyhin <a.busyhin@urancompany.com>
2022-03-30 11:16:24 +0300

    set prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,19 @@
+require("dotenv").config();
+
 module.exports = {
   siteMetadata: {
     title: ``,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [{
-    resolve: 'gatsby-source-shopify',
-    options: {
-      "shopName": "uran-gatsby-shopify-project",
-      "accessToken": ""
-    }
-  }, "gatsby-plugin-styled-components"]
+  plugins: [
+    {
+      resolve: 'gatsby-source-shopify',
+      options: {
+        password: process.env.GATSBY_SHOPIFY_PASSWORD,
+        storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
+      }
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+  ]
 };
